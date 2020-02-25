785de62ee7509ed9109c35dbf752cfc0a62ea85e
Student <example@example.com>
2020-02-25 09:49:53 -0800

    add formatting

--- Triangle.cs
+++ Triangle.cs
@@ -7,53 +7,51 @@ public class Triangle
     public int Side2;
     public int Side3;
     public Triangle(int side1, int side2, int side3)
-  {
-  Side1 = side1;
-  Side2 = side2;
-  Side3 = side3;
-  }
+    {
+        Side1 = side1;
+        Side2 = side2;
+        Side3 = side3;
+    }
 }
 
 public class Program
 {
-public static void Main()
-{
+    public static void Main()
+    {
 
-Console.WriteLine("Enter side 1: ");
-string stringSide1 = Console.ReadLine();
-int intSide1 = int.Parse(stringSide1);
+        Console.WriteLine("Enter side 1: ");
+        string stringSide1 = Console.ReadLine();
+        int intSide1 = int.Parse(stringSide1);
 
-Console.WriteLine("Enter side 2: ");
-string stringSide2 = Console.ReadLine();
-int intSide2 = int.Parse(stringSide2);
+        Console.WriteLine("Enter side 2: ");
+        string stringSide2 = Console.ReadLine();
+        int intSide2 = int.Parse(stringSide2);
 
-Console.WriteLine("Enter side 3: ");
-string stringSide3 = Console.ReadLine();
-int intSide3 = int.Parse(stringSide3);
+        Console.WriteLine("Enter side 3: ");
+        string stringSide3 = Console.ReadLine();
+        int intSide3 = int.Parse(stringSide3);
 
-Triangle userTriangle = new Triangle(intSide1, intSide2, intSide3);
-TriangleTracker();
+        Triangle userTriangle = new Triangle(intSide1, intSide2, intSide3);
+        TriangleTracker();
 
-void TriangleTracker()
-{
-  if (userTriangle.Side1 + userTriangle.Side2 <= userTriangle.Side3 || userTriangle.Side2 + userTriangle.Side3 <= userTriangle.Side1 || userTriangle.Side3 + userTriangle.Side1 <= userTriangle.Side2)
-  {
-    Console.WriteLine("Not a Triangle! Try again!");
-  }
-  else if (userTriangle.Side1 == userTriangle.Side2 && userTriangle.Side2 == userTriangle.Side3)
-  {
-    Console.WriteLine("You have an Equilateral Triangle!");
-  }
-  else if (userTriangle.Side1 == userTriangle.Side2 && userTriangle.Side2 != userTriangle.Side3 || userTriangle.Side1 == userTriangle.Side3 && userTriangle.Side1 != userTriangle.Side2 || userTriangle.Side2 == userTriangle.Side3 && userTriangle.Side2 != userTriangle.Side1)
-  {
-    Console.WriteLine("You have an Isosceles Triangle!");
-  }
-  else if (userTriangle.Side1 != userTriangle.Side2 && userTriangle.Side2 != userTriangle.Side3 && userTriangle.Side3 != userTriangle.Side1)
-  {
-    Console.WriteLine("You have an Scalene Triangle!");
-  }
+        void TriangleTracker()
+        {
+            if (userTriangle.Side1 + userTriangle.Side2 <= userTriangle.Side3 || userTriangle.Side2 + userTriangle.Side3 <= userTriangle.Side1 || userTriangle.Side3 + userTriangle.Side1 <= userTriangle.Side2)
+            {
+                Console.WriteLine("Not a Triangle! Try again!");
+            }
+            else if (userTriangle.Side1 == userTriangle.Side2 && userTriangle.Side2 == userTriangle.Side3)
+            {
+                Console.WriteLine("You have an Equilateral Triangle!");
+            }
+            else if (userTriangle.Side1 == userTriangle.Side2 && userTriangle.Side2 != userTriangle.Side3 || userTriangle.Side1 == userTriangle.Side3 && userTriangle.Side1 != userTriangle.Side2 || userTriangle.Side2 == userTriangle.Side3 && userTriangle.Side2 != userTriangle.Side1)
+            {
+                Console.WriteLine("You have an Isosceles Triangle!");
+            }
+            else if (userTriangle.Side1 != userTriangle.Side2 && userTriangle.Side2 != userTriangle.Side3 && userTriangle.Side3 != userTriangle.Side1)
+            {
+                Console.WriteLine("You have an Scalene Triangle!");
+            }
+        }
+    }
 }
-}
-
-}
-    
